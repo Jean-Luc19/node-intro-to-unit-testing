e1c43dd2e5a58fbb3808a2499c02115711ea0682
Jean-Luc19 <aticus19@gmail.com>
2017-02-21 09:33:04 -0700

    added fizzbuzzer test file

---
+++ test/test-fizzbuzzer.js
@@ -0,0 +1,16 @@
+const should = require('chai').should();
+
+const fizzBuzzer = require('../fizzBuzzer');
+
+
+
+
+
+
+
+// describe('Tests for fizz-buzzbuzzer function', function(){
+//   it('should throw an error if the input is not a number', function(){
+//     const answer = fizzBuzzer([4, 4]);
+//     answer.should.be('error');
+//   });
+// })
