6aa76fb107814c585490812ec819686aecbd9fa4
Jean-Luc19 <aticus19@gmail.com>
2017-02-21 09:57:06 -0700

    fizbuzz and fizz tests working

--- test/test-fizzbuzzer.js
+++ test/test-fizzbuzzer.js
@@ -2,15 +2,31 @@ const should = require('chai').should();
 
 const fizzBuzzer = require('../fizzBuzzer');
 
+describe('fizzBuzzer', function() {
+  it('should return FizzBuzz when num % 15 is 0', function(){
+    const normalCases = [15, 45, 90];
+    normalCases.forEach(x => {
+      const answer = fizzBuzzer(x);
+      answer.should.equal('fizz-buzz');
+    });
+  });
+
+  it('should return fizz when num % 3 is 0', function() {
+    const normalCases = [3, 9, 12];
+    normalCases.forEach(x => {
+      const answer = fizzBuzzer(x);
+      answer.should.equal('fizz');
+    })
+  });
+});
 
 
 
 
 
-
-// describe('Tests for fizz-buzzbuzzer function', function(){
+// describe('fizzBuzzer', function(){
 //   it('should throw an error if the input is not a number', function(){
 //     const answer = fizzBuzzer([4, 4]);
-//     answer.should.be('error');
+//     answer.should.throw(Error);
 //   });
-// })
+// });
